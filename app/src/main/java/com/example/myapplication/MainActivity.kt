082df2b2f6df8b35be0e64bb8e3b2c1c440b931f
gukhman <gukhman@gmail.com>
2024-08-13 12:27:29 +0200

    reverse 13.08

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textInput: EditText
+    private lateinit var textResult: TextView
+    private lateinit var textCount: TextView
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        textInput = findViewById(R.id.textInput)
+        textResult = findViewById(R.id.textResult)
+        textCount = findViewById(R.id.textCount)
+        button = findViewById(R.id.button)
+    }
+
+    fun onButtonClick(view: View) {
+        textResult.text = textInput.text.reversed()
+        textCount.text = textInput.text.filterNot { it == ' ' }.length.toString()
+    }
+
+}
